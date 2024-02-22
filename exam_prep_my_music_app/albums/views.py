434ae0b92fda60f3940257ed53496071858512ea
Tonkooo <at.antonio.todorov@gmail.com>
2024-02-23 00:41:54 +0200

    exam prep finished

--- exam_prep_my_music_app/albums/views.py
+++ exam_prep_my_music_app/albums/views.py
@@ -1,8 +1,8 @@
+from django.forms import modelform_factory
 from django.urls import reverse_lazy
 from django.views import generic as views
 
 from exam_prep_my_music_app.albums.models import Album
-from django import forms
 
 from exam_prep_my_music_app.profiles.models import Profile
 
@@ -11,21 +11,45 @@ def get_profile():
     return Profile.objects.first()
 
 
-class CreateAlbumView(views.CreateView):
-    queryset = Album.objects.all()
-    fields = ("name", "artist_name", "genre", "description", "image_url", "price")
-    template_name = "albums/album-add.html"
-    success_url = reverse_lazy("index")
-
+class AlbumFormMixin:
     def get_form(self, form_class=None):
         form = super().get_form(form_class=form_class)
-
         form.fields["name"].widget.attrs["placeholder"] = "Album Name"
         form.fields["artist_name"].widget.attrs["placeholder"] = "Artist"
         form.fields["description"].widget.attrs["placeholder"] = "Description"
         form.fields["image_url"].widget.attrs["placeholder"] = "Image URL"
         form.fields["price"].widget.attrs["placeholder"] = "Price"
         return form
+# text in the form fields
+
+
+class ReadonlyviewMixin:
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class=form_class)
+
+        for field in form.fields.values():
+            field.widget.attrs["readonly"] = "readonly"
+            #field.widget.attrs["disabled"] = "disabled"
+
+        return form
+# when delete form text in the fields cannot be deleted.
+
+class CreateAlbumView(AlbumFormMixin, views.CreateView):
+    queryset = Album.objects.all()
+    fields = ("name", "artist_name", "genre", "description", "image_url", "price")
+    template_name = "albums/album-add.html"
+    success_url = reverse_lazy("index")
+
+    # def get_form(self, form_class=None):
+    #     form = super().get_form(form_class=form_class)
+    #
+    #     form.fields["name"].widget.attrs["placeholder"] = "Album Name"
+    #     form.fields["artist_name"].widget.attrs["placeholder"] = "Artist"
+    #     form.fields["description"].widget.attrs["placeholder"] = "Description"
+    #     form.fields["image_url"].widget.attrs["placeholder"] = "Image URL"
+    #     form.fields["price"].widget.attrs["placeholder"] = "Price"
+    #     return form
 
 
     def form_valid(self, form):
@@ -38,7 +62,25 @@ class DetailAlbumView(views.DetailView):
     template_name = "albums/album-details.html"
 
 
-class EditAlbumView(views.UpdateView):
+class EditAlbumView(AlbumFormMixin, views.UpdateView):
     queryset = Album.objects.all()
     template_name = "albums/album-edit.html"
     fields = ("name", "artist_name", "genre", "description", "image_url", "price")
+    success_url = reverse_lazy("index")  # redirect to index page
+
+
+class DeleteAlbumView(ReadonlyviewMixin, views.DeleteView):
+    queryset = Album.objects.all()
+    template_name = "albums/album-delete.html"
+    success_url = reverse_lazy("index")  # redirect to index page
+    form_class = modelform_factory(Album, fields=("name", "artist_name", "genre", "description", "image_url", "price"))
+    # with this form_class we bring the whole album form fields. If this is missing, there
+    # is only "delete" button
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs["instance"] = self.object
+        return kwargs                          # this kwargs fills in the form with the album information
+
+
+
--- exam_prep_my_music_app/profiles/views.py
+++ exam_prep_my_music_app/profiles/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.views import generic as views
+from exam_prep_my_music_app.albums.models import Album
+from exam_prep_my_music_app.profiles.models import Profile
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+def get_profile():
+    return Profile.objects.first()
+
+
+class DetailProfileView(views.DetailView):
+    queryset = Profile.objects.all()
+    template_name = 'profiles/profile-details.html'
+
+    def get_object(self, queryset=None):
+        return get_profile()
+
+
+class DeleteProfileView(views.DeleteView):
+    template_name = "profiles/profile-delete.html"
+    success_url = reverse_lazy("index")
+
+    def get_object(self, queryset=None):
+        return get_profile()
